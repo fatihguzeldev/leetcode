9d7d3e280887289dae5a497ad24b692759f7e11f
fatihguzel1 <fatih@decktopus.com>
2024-02-29 13:01:59 +0300

    two-sum

---
+++ 1.two-sum/1.two-sum.ts
@@ -0,0 +1,22 @@
+/*
+ * @lc app=leetcode id=1 lang=typescript
+ *
+ * [1] Two Sum
+ */
+
+// @lc code=start
+function twoSum(nums: number[], target: number): number[] {
+  const indicies = new Map<number, number>();
+
+  for (let i = 0; i < nums.length; i++) {
+    const pair = target - nums[i];
+
+    if (indicies.has(pair)) {
+      return [indicies.get(pair), i];
+    }
+
+    indicies.set(nums[i], i);
+  }
+}
+
+// @lc code=end
