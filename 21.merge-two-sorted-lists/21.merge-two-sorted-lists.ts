b794f90a4a27d6e80b4d09e71eff0e2a5fe696e7
fatihguzel <hello@fatihguzel.dev>
2025-07-10 12:18:53 +0300

    merge two sorted lists

--- 21.merge-two-sorted-lists/21.merge-two-sorted-lists.ts
+++ 21.merge-two-sorted-lists/21.merge-two-sorted-lists.ts
@@ -6,14 +6,14 @@
 
 // @lc code=start
 
-// class ListNode {
-//   val: number;
-//   next: ListNode | null;
-//   constructor(val?: number, next?: ListNode | null) {
-//     this.val = val === undefined ? 0 : val;
-//     this.next = next === undefined ? null : next;
-//   }
-// }
+class ListNode {
+  val: number;
+  next: ListNode | null;
+  constructor(val?: number, next?: ListNode | null) {
+    this.val = val === undefined ? 0 : val;
+    this.next = next === undefined ? null : next;
+  }
+}
 
 function mergeTwoLists(
   list1: ListNode | null,
