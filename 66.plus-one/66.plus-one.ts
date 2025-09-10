4bc69120a2a31888948ae89c69a4e65f269d6c88
fatihguzel <fatihguzeldev@gmail.com>
2025-09-10 13:56:14 +0300

    plus one

---
+++ 66.plus-one/66.plus-one.ts
@@ -0,0 +1,27 @@
+/*
+ * @lc app=leetcode id=66 lang=typescript
+ *
+ * [66] Plus One
+ */
+
+// @lc code=start
+function plusOne(digits: number[]): number[] {
+  let carry = 1;
+
+  for (let i = digits.length - 1; i >= 0; i--) {
+    const sum = digits[i] + carry;
+    digits[i] = sum % 10;
+    carry = Math.floor(sum / 10);
+  }
+
+  if (carry) {
+    for (let i = digits.length - 1; i >= 0; i--) {
+      digits[i + 1] = digits[i];
+    }
+
+    digits[0] = carry;
+  }
+
+  return digits;
+}
+// @lc code=end
--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-Solutions for various problems on LeetCode.
+regrets, i've had a few
+but then again, too few to mention
+i did what i had to do
+and saw it through without exemption
+i planned each charted course
+each careful step along the byway
+and more, much more than this
+i did it my way
