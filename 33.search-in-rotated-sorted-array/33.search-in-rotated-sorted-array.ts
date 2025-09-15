7889c1ac45ba4464d111424b935c2d5fb0c95c07
fatihguzel <fatihguzeldev@gmail.com>
2025-09-15 14:03:18 +0300

    search in rotated sorted array

---
+++ 33.search-in-rotated-sorted-array/33.search-in-rotated-sorted-array.ts
@@ -0,0 +1,38 @@
+/*
+ * @lc app=leetcode id=33 lang=typescript
+ *
+ * [33] Search in Rotated Sorted Array
+ */
+
+// @lc code=start
+function search(nums: number[], target: number): number {
+  let left = 0;
+  let right = nums.length - 1;
+
+  while (left <= right) {
+    const mid = Math.floor((left + right) / 2);
+
+    if (target === nums[mid]) {
+      return mid;
+    }
+
+    if (nums[left] <= nums[mid]) {
+      if (nums[left] <= target && target < nums[mid]) {
+        right = mid - 1;
+      } else {
+        left = mid + 1;
+      }
+    }
+
+    if (nums[mid] <= nums[right]) {
+      if (nums[mid] < target && target <= nums[right]) {
+        left = mid + 1;
+      } else {
+        right = mid - 1;
+      }
+    }
+  }
+
+  return -1;
+}
+// @lc code=end
