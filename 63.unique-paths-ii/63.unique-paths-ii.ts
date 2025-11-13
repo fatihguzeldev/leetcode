33d5659e8a0c453add27d40c35c938885085fdf2
fatihguzel <fatihguzeldev@gmail.com>
2025-11-13 15:02:18 +0300

    unique paths ii

--- 63.unique-paths-ii/63.unique-paths-ii.ts
+++ 63.unique-paths-ii/63.unique-paths-ii.ts
@@ -9,7 +9,7 @@ function uniquePathsWithObstacles(obstacleGrid: number[][]): number {
   const m = obstacleGrid.length;
   const n = obstacleGrid[0].length;
 
-  if (obstacleGrid[m - 1][n - 1] === 1 || obstacleGrid[0][0] === 1) {
+  if (obstacleGrid[m - 1][n - 1] === 1) {
     return 0;
   }
 
