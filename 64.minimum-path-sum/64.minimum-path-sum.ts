bb068fb9046a136f35bb6411a9fcccccbae3c8f1
fatihguzel <fatihguzeldev@gmail.com>
2025-11-14 17:51:58 +0300

    minimum path sum

---
+++ 64.minimum-path-sum/64.minimum-path-sum.ts
@@ -0,0 +1,27 @@
+/*
+ * @lc app=leetcode id=64 lang=typescript
+ *
+ * [64] Minimum Path Sum
+ */
+
+// @lc code=start
+function minPathSum(grid: number[][]): number {
+  const rows = grid.length;
+  const columns = grid[0].length;
+
+  for (let i = 0; i < rows; i++) {
+    for (let j = 0; j < columns; j++) {
+      if (i === 0 && j === 0) {
+        continue;
+      }
+
+      const top = i > 0 ? grid[i - 1][j] : Infinity;
+      const right = j > 0 ? grid[i][j - 1] : Infinity;
+
+      grid[i][j] += Math.min(top, right);
+    }
+  }
+
+  return grid[rows - 1][columns - 1];
+}
+// @lc code=end
