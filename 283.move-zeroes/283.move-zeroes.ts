aca059fb287f1c0493aa2e66ce5b2d2829cf4e66
fatihguzel <fatihguzeldev@gmail.com>
2026-01-06 15:39:58 +0300

    move zeroes

---
+++ 283.move-zeroes/283.move-zeroes.ts
@@ -0,0 +1,27 @@
+/*
+ * @lc app=leetcode id=283 lang=typescript
+ *
+ * [283] Move Zeroes
+ */
+
+// @lc code=start
+/**
+ Do not return anything, modify nums in-place instead.
+ */
+function moveZeroes(nums: number[]): void {
+  let insertPos = 0;
+
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] !== 0) {
+      nums[insertPos] = nums[i];
+      insertPos++;
+    }
+  }
+
+  while (insertPos < nums.length) {
+    nums[insertPos] = 0;
+    insertPos++;
+  }
+};
+// @lc code=end
+
