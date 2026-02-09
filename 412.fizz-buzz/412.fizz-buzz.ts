e07864fdce0559248070c631634d96174bed8618
fatihguzel <fatihguzeldev@gmail.com>
2026-02-09 14:54:58 +0300

    fizz buzz

---
+++ 412.fizz-buzz/412.fizz-buzz.ts
@@ -0,0 +1,26 @@
+/*
+ * @lc app=leetcode id=412 lang=typescript
+ *
+ * [412] Fizz Buzz
+ */
+
+// @lc code=start
+function fizzBuzz(n: number): string[] {
+  let res: string[] = [];
+
+  for (let i = 1; i <= n; i++) {
+    if (i % 15 === 0) {
+      res.push("FizzBuzz");
+    } else if (i % 3 === 0) {
+      res.push("Fizz");
+    } else if (i % 5 === 0) {
+      res.push("Buzz");
+    } else {
+      res.push(String(i));
+    }
+  }
+
+  return res;
+};
+// @lc code=end
+
